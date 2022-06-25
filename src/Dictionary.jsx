8b0819ff65b24f194bece8cbc41c9bc999508057
samikshyakc1996 <samikshyakc1996@gmail.com>
2022-06-25 10:37:55 -0400

    installed axios and implemented the oxford api

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,10 +1,18 @@
 import {React, useState} from 'react'
-
+import axios from "axios"
 const Dictionary = () => {
     const [keyword, setKeyword]=useState("");
+
+    function handleResponse(response){
+        console.log(response);
+    }
+
+    //Documentation: apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/pickles`
     function handleSearch(e){
         e.preventDefault();
-        alert(`Searching for ${keyword}`);
+        // alert(`Searching for ${keyword}`);
+        const apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event){
         setKeyword(event.target.value);
