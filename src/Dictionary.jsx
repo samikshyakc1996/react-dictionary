3932d02f8d8d1ce5c95e7c490d170b697a34ff84
samikshyakc1996 <samikshyakc1996@gmail.com>
2022-06-28 08:54:55 -0400

    added sunset as a default keyword to search for

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -3,36 +3,51 @@ import axios from "axios"
 import Result from './Result';
 
 const Dictionary = () => {
-    const [keyword, setKeyword]=useState("");
+    const [keyword, setKeyword]=useState("sunset");
     const [result,setResult]=useState(null);
+    const [searched,setSearched]=useState(false);
+
     function handleResponse(response){
         console.log(response);
         setResult(response);
     }
 
+    function search(){
+        const apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
+        setSearched(true);
+    }
+
     //Documentation: apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/pickles`
     function handleSearch(e){
         e.preventDefault();
-        // alert(`Searching for ${keyword}`);
-        const apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        search();
+       
     }
     function handleKeywordChange(event){
         setKeyword(event.target.value);
     }
-  return (
-    <div className='Dictionary'>
-        <form onSubmit={handleSearch} 
-                style={{display: "flex",
-                justifyContent: "center",
-                alignItems: "center"}}>
-            <input type="search"  placeholder='Type a word . . ' onChange={handleKeywordChange}
-             />
-        </form>
-       
-            <Result result={result}/>
-    </div>
-  )
+
+    if(searched){
+        return (
+            <div className='Dictionary'>
+                <form onSubmit={handleSearch} 
+                        style={{display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"}}>
+                    <input type="search"  placeholder='Type a word . . ' onChange={handleKeywordChange}
+                     />
+                </form>
+               
+                    <Result result={result}/>
+            </div>
+          )
+    }
+    else{
+        search();
+        return "Loading"
+    }
+  
 }
 
 export default Dictionary
